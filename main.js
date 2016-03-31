1707a7b2fd5f8e8da13964a8a7b136a59c63f449
Ben Ebel <bge1234@gmail.com>
2016-03-31 13:37:58 -0600

    change formatting

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-function fib(n) {
-  return n === 0 ? 0 : ((n === 1 || n === 2) ? 1 : fib(n - 1) + fib(n - 2));
-}
+function fib(n) { return n === 0 ? 0 : ((n === 1 || n === 2) ? 1 : fib(n - 1) + fib(n - 2)) }
 
 console.log(fib(8));

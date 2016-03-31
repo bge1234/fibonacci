4877daad0382ae4a26f796717f7d60fa9024b902
Ben Ebel <bge1234@gmail.com>
2016-03-31 13:49:22 -0600

    removed case for n = 2 because that can be found recursively

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function fib(n) { return n === 0 ? 0 : ((n === 1 || n === 2) ? 1 : fib(n - 1) + fib(n - 2)) }
+function fib(n) { return n === 0 ? 0 : (n === 1 ? 1 : fib(n - 1) + fib(n - 2)) }
 
 console.log(fib(0));
 console.log(fib(1));

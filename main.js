ad6b52c160eb1d32ffd36b9661e97e66a327a5f8
Ben Ebel <bge1234@gmail.com>
2016-03-31 14:23:15 -0600

    modified output formatting

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 function fib(n) { return n === 0 ? 0 : (n === 1 ? 1 : fib(n - 1) + fib(n - 2)) }
 
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-console.log(fib(5));
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
+console.log("  n = 0, 1, 2, 3, 4, 5, 6,  7,  8,  9, 10");
+console.log("-----------------------------------------");
+console.log("fib = " + fib(0) + ", " + fib(1) + ", " + fib(2) + ", " + fib(3) + ", " + fib(4) + ", " + fib(5) + ", " + fib(6) + ", " + fib(7) + ", " + fib(8) + ", " + fib(9) + ", " + fib(10));

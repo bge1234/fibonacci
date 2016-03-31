0ddacfbdba96cd2b5fd607cea8a7dc60fcced356
Ben Ebel <bge1234@gmail.com>
2016-03-31 13:38:46 -0600

    added console logs to spit out entire sequence 0-8

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function fib(n) { return n === 0 ? 0 : ((n === 1 || n === 2) ? 1 : fib(n - 1) + fib(n - 2)) }
 
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(7));
 console.log(fib(8));
